92e223686f4df08315c6e478199d79034e29c795
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-07-20 00:33:45 +0530

    Backend - Mongo DB professional approach

---
+++ 3. Backend/src/index.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants.js'
+import connectDB from './db/index.js';
+
+// // Approach 1
+// import express from 'express'
+// const app = express()
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Error: ",error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     } catch(error) {
+//         console.error("Error: ",error)
+//         throw err
+//     }
+// })()
+
+// Approach 2 - More Professional
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
